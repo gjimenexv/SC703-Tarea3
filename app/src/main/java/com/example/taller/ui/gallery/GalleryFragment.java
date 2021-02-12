81f80a7ae5a1dbea7ce4ea1c76b23282f0857268
Guillermo Jimenez <guillejim5@gmail.com>
2021-02-11 22:42:48 -0600

    Tarea 3

--- app/src/main/java/com/example/taller/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/taller/ui/gallery/GalleryFragment.java
@@ -2,14 +2,18 @@ package com.example.taller.ui.gallery;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.pdf.PdfRenderer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,7 +42,6 @@ public class GalleryFragment extends Fragment {
     private Button btDescargar;
     private Button btBuscar;
     private Button btCarga;
-    private ImageView imagen;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -51,9 +54,6 @@ public class GalleryFragment extends Fragment {
         btDescargar = root.findViewById(R.id.btDescargar);
         btBuscar = root.findViewById(R.id.btBuscar);
         btCarga = root.findViewById(R.id.btCargar);
-        imagen = root.findViewById(R.id.imagen);
-
-
 
         btDescargar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +82,7 @@ public class GalleryFragment extends Fragment {
         String archivo = etArchivo.getText().toString();
         if (archivo.isEmpty()){
             etArchivo.requestFocus();
-            Toast.makeText(getContext(), "Escriba un nombre para la foto", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "Escriba un nombre para el documento", Toast.LENGTH_SHORT).show();
         }else{
             StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(archivo);
             storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
@@ -103,7 +103,7 @@ public class GalleryFragment extends Fragment {
         //Utilizamos un file choose de Android
         etArchivo.setText("");
         Intent intento = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intento.setType("image/*");
+        intento.setType("application/pdf");
         startActivityForResult(intento,333);
 
     }
@@ -112,29 +112,29 @@ public class GalleryFragment extends Fragment {
     //onActivityResult captura el resultado de la busqueda del Intent anterior.
     public void onActivityResult(int requestCode, int resultCode, Intent datos){
         uri = datos.getData();
-        imagen.setImageURI(uri);
-
+        Toast.makeText(getContext(), "Archivo Seleccionado", Toast.LENGTH_SHORT).show();
     }
-
 
     private void descargar() {
         String archivo = etArchivo.getText().toString();
-        //Referencia a un elemento que esta en el storage
-        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(archivo);
+        if (archivo.isEmpty()){
+            etArchivo.requestFocus();
+            Toast.makeText(getContext(), "Escriba el nombre del documento que busca", Toast.LENGTH_SHORT).show();
+        }else{
+            //Referencia a un elemento que esta en el storage
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference().child(archivo);
+            String name = storageReference.getName();
+            File localFile = null; //Archivo local temp.
 
-        File localFile = null; //Archivo local temp.
-
-        try {
-            localFile = File.createTempFile("images", "jpg");
-            String ubicacion = localFile.getAbsolutePath(); //Ruta del archivo temporal
-
+            localFile = new File(String.valueOf(getContext().getFilesDir()),archivo+".pdf");
 
             //Descargo la imagen de manera asincrona
+            File finalLocalFile = localFile;
             storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                 @Override
                 public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                    imagen.setImageBitmap(BitmapFactory.decodeFile(ubicacion));
-
+                    String ubicacion = finalLocalFile.getAbsolutePath(); //Ruta del archivo temporal
+                    Toast.makeText(getContext(), "Descarga completada: "+ubicacion, Toast.LENGTH_SHORT).show();
                 }
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
@@ -142,10 +142,6 @@ public class GalleryFragment extends Fragment {
                     Toast.makeText(getContext(), "Error Descargando...", Toast.LENGTH_SHORT).show();
                 }
             });
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-
     }
 }
